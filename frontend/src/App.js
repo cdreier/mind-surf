826ca09f792705631fc3d06af34fa51a764273c9
Christian Dreier <dreier.christian@googlemail.com>
2018-03-16 20:46:33 +0100

    a bit of styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,13 +14,75 @@ const Wrapper = styled.div`
   position: absolute;
 `
 
+// const svgSquare = {
+//   shape: 'rect',
+//   shapeProps: {
+//     width: 130,
+//     height: 30,
+//     x: -10,
+//     y: -10,
+//   },
+// }
+
+const textLayout = {
+  textAnchor: 'start', 
+  x: 0, 
+  y: 10, 
+  transform: undefined,
+}
+
+const treeStyle = {
+  nodes: {
+    node: {
+      circle: {
+        fill: '#6ab7ff',
+        stroke: '#005cb2',
+        strokeWidth: 1,
+      },
+      name: {
+        fontSize: 14,
+        width: 130,
+        style: {
+          overflow: 'hidden',
+          'text-overflow': 'ellipsis',
+        },
+      },
+    },
+    leafNode: {
+      circle: {
+        fill: '#b2fab4',
+        stroke: '#519657',
+        strokeWidth: 1,
+      },
+      name: {
+        fontSize: 14,
+        width: 130,
+        style: {
+          overflow: 'hidden',
+          'text-overflow': 'ellipsis',
+        },
+      },
+    },
+  },
+}
+
+const separation = { 
+  siblings: 1.3, 
+  nonSiblings: 3,
+}
+
+
 @inject('tree')
 @observer
 class App extends Component {
   render(){
     return (
       <Wrapper >
-        <Tree data={mobx.toJS(this.props.tree.data)} />
+        <Tree 
+          separation={separation}
+          styles={treeStyle}
+          textLayout={textLayout}
+          data={mobx.toJS(this.props.tree.data)} />
       </Wrapper>
     )
   }
